2d4e2ec6ef712a0037fed799bc8ff4a19b9c3625
Gabriel Kutik <gabrielkutik@yahoo.com>
2019-08-04 23:00:39 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,16 @@
 # Add your variables here
+def sum(first_number, second_number)
+  first_number + second_number
+end
+
+def difference(first_number, second_number)
+  first_number - second_number
+end
+
+def product(first_number, second_number)
+  first_number * second_number
+end
+
+def quotient(first_number, second_number)
+  first_number / second_number
+end
